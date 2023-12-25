ea7e1413b0f3b9a8eb13d2fc32232753d4bcd041
Niklas Johansson <dwarfley.private@gmail.com>
2023-12-25 12:25:18 +0100

    Create gradle project

---
+++ src/main/java/io/github/dwarfley/gradle/lwjglhelper/LwjglHelperPlugin.java
@@ -0,0 +1,12 @@
+package io.github.dwarfley.gradle.lwjglhelper;
+
+import org.gradle.api.*;
+
+public class LwjglHelperPlugin implements Plugin<Project> {
+	
+	@Override
+	public void apply(Project pProject){
+		
+	}
+	
+}
